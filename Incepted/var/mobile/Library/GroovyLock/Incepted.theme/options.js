a0ef428e4e0b8c52f109d4e0ff701f8c077fa8f2
Jesse Bryan <winneonsword@gmail.com>
2014-04-04 21:32:25 +0200

    Updated Incepted.

---
+++ Incepted/var/mobile/Library/GroovyLock/Incepted.theme/options.js
@@ -0,0 +1,7 @@
+// true = 12 hours (i.e. 16:00)
+// false = 24 hours (i.e. 4:00 PM)
+var hours12format = false;
+
+// true = show time in text format (i.e. twelve.o'clock)
+// false = show time in number format (i.e. 12:00)
+var textFormat = true;
