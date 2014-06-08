0792c40afd1e1dd1a5b5f17de0a4e626dbb8b1ea
Jesse Bryan <winneonsword@gmail.com>
2014-06-08 03:48:24 +0200

    Added new LS theme.

---
+++ Slide/var/mobile/Library/GroovyLock/Slide.theme/script.js
@@ -0,0 +1,6 @@
+var verbal = new VerbalDate(false);
+var date = new Date();
+
+$(".date").html(date.getDate() + " " + verbal.getMonth() + " " + date.getFullYear());
+$(".hours").html(date.getHours());
+$(".mins").html(date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes());
--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ dpkg-deb -b Minimality
 dpkg-deb -b CircleLove
 dpkg-deb -b Incepted
 dpkg-deb -b Clockwork
+dpkg-deb -b Slide
 
 dpkg-scanpackages -m . /dev/null >Packages
 bzip2 Packages
