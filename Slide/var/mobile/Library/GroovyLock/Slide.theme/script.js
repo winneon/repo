15226eaf816cf26a09acf1135221f2d60425920f
Jesse Bryan <winneonsword@gmail.com>
2014-06-10 18:32:42 +0200

    Added 12 hours options to Slide.

--- Slide/var/mobile/Library/GroovyLock/Slide.theme/script.js
+++ Slide/var/mobile/Library/GroovyLock/Slide.theme/script.js
@@ -3,9 +3,14 @@ run();
 function run(){
 	var verbal = new VerbalDate(false);
 	var date = new Date();
+	var hours = date.getHours();
+	
+	if (!(hours24)){
+		hours = hours > 12 ? hours - 12 : hours;
+	}
 	
 	$(".date").html(date.getDate() + " " + verbal.getMonth() + " " + date.getFullYear());
-	$(".hours").html(date.getHours());
+	$(".hours").html(hours > 9 ? hours : "0" + hours);
 	$(".mins").html(date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes());
 	setTimeout("run();", 1000);
 }
