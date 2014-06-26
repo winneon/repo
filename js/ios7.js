7134dd1561ad90321fe7e7e24c7d98f2c91d8b50
Jesse Bryan <winneonsword@gmail.com>
2014-06-26 12:36:25 -0500

    Screenshots@

--- js/ios7.js
+++ js/ios7.js
@@ -1,9 +1,13 @@
 // Basic iOS 7 CSS // Apache 2 license // hbang.ws
-(function (a){
+var split = document.title.split(" \u00b7 ")[0];
+$("header h1").html(split);
+$("a").attr("target", "_blank");
+$("img").parent().addClass("screens");
+(function (a, b){
 	"use strict";
 	navigator.userAgent.indexOf("Cydia") != -1 ?
 		(
-			a.title = a.title.split(" \u00b7 ")[0],
+			a.title = b,
 			a.documentElement.classList.add("cydia", "depiction")
 		) : a.documentElement.classsList.remove("cydia", "depiction");
-})(document);
+})(document, split);
