8a32d1134355472cf0690ca54541242de8251811
Jesse Bryan <winneonsword@gmail.com>
2014-07-06 15:43:47 -0500

    Fixed the naming.

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ dpkg-deb -b -Zgzip "Clockwork"
 dpkg-deb -b -Zgzip "Slide"
 dpkg-deb -b -Zgzip "LCD"
 dpkg-deb -b -Zgzip "Cataracs"
-dpkg-deb -b -Zgzip "Cataracs for iPad"
+dpkg-deb -b -Zgzip "CataracsforiPad"
 
 dpkg-scanpackages -m . /dev/null > Packages
 bzip2 Packages
