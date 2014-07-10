35848a5966cab41f0f210a397c01498ad22a15b3
Jesse Bryan <winneonsword@gmail.com>
2014-07-10 16:00:35 -0500

    Updated Cataracs to 1.1.

--- Cataracs/var/mobile/Library/GroovyLock/Cataracs.theme/script.js
+++ Cataracs/var/mobile/Library/GroovyLock/Cataracs.theme/script.js
@@ -8,10 +8,13 @@ function run(){
 	
 	if (!(hours24)){
 		hours = hours > 12 ? hours - 12 : hours;
+		if (hours == 0){
+			hours = 12;
+		}
 	}
 	
 	$(".hours").html(hours > 9 ? hours : "0" + hours);
 	$(".mins").html(date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes());
 	
-	setTimeout("run();", 1000);
+	setTimeout(run, 1000);
 }
