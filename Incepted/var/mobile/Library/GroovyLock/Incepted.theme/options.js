c2e7635b106e9ca6f5186565d03c2acfda2442dd
spencersmith <spencer@koddi.com>
2015-03-24 01:15:11 -0500

    Created the ability to use american military phrasing.
    Adds american military phrasing for minutes and hours.
    Adds a variable in the options config file.

--- Incepted/var/mobile/Library/GroovyLock/Incepted.theme/options.js
+++ Incepted/var/mobile/Library/GroovyLock/Incepted.theme/options.js
@@ -5,3 +5,8 @@ var hours12format = false;
 // true = show time in text format (i.e. twelve.o'clock)
 // false = show time in number format (i.e. 12:00)
 var textFormat = true;
+
+// true = show words using american military phrasing (i.e. oh one hundred)
+// false = use normal language
+// You should obviously be on 24 hour time (hours12format = false) for this
+var militaryTime = true;
